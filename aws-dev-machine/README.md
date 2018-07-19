02e0f5241c4fc1ac92eac2864bdfdcb567d5865e
Mike Miklin <MiklinMA@gmail.com>
2018-07-19 12:49:41 +0500

    AWS dev machine control

---
+++ aws-dev-machine/README.md
@@ -0,0 +1,14 @@
+# AWS devel machine control
+
+## Commands
+
+1. Default (no params) - short status message (name and state)
+
+2. Status - full status message
+
+3. IP - ip address of machine
+
+4. Start
+
+5. Stop
+
---
+++ aws-dev-machine/aws-dev-machine.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+# 
+# 2018
+# Mike Miklin <MiklinMA@gmail.com>
+#
+# Give an access for any inbound traffic 
+# on service group for public IP of local machine
+# 
+
+inst="i-0f4bacddf2d743c6f"
+
+start() {
+    echo "Start $inst"
+    aws ec2 start-instances \
+        --instance-ids $inst \
+        --output text
+}
+
+stop() {
+    echo "Stop $inst"
+    aws ec2 stop-instances \
+        --instance-ids $inst \
+        --output text
+}
+
+status() {
+    echo "Status $1"
+
+    res=$(aws ec2 describe-instances \
+        --instance-ids $inst \
+        --output text)
+
+    if [ -z "$1" ]; then
+        echo "$res" | grep "STATE\s\|TAGS\sName"
+
+    elif [ "$1" == "ip" ]; then
+        echo "$res" \
+            | grep -A1 "NETWORKINTERFACES\s" \
+            | grep "ASSOCIATION\s" \
+            | awk '{print $4}'
+
+    else
+        echo "$res"
+    fi
+}
+
+case $1 in
+    "start")
+        start
+        ;;
+    "stop")
+        stop
+        ;;
+    "status")
+        status full
+        ;;
+    "ip")
+        status ip
+        ;;
+    *)
+        status
+        ;;
+esac
