edb5852851f298207ef7414f62962c81905c3e00
Mike Miklin <MiklinMA@gmail.com>
2018-07-19 17:00:51 +0500

    Groovy query

--- http2https/README.md
+++ http2https/README.md
@@ -1,4 +1,11 @@
 # Simple redirect from HTTP to HTTPs
 
-Useful to redirect Jenkins traffic from HTTP to HTTPs without Apache or Nginx
+Useful to redirect Jenkins traffic from HTTP to HTTPs without Apache or Nginx.
+
+On AWS EC2 you can use IPTables to redirect depending on root access port 80 to 8888 like this:
+
+```
+/sbin/iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80  -j REDIRECT --to-port 8888
+/sbin/iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 8443
+```
 
--- http2https/http2https.groovy
+++ http2https/http2https.groovy
@@ -14,15 +14,18 @@ def server = HttpServer.create(new InetSocketAddress(port), 0)
 
 server.createContext("/", 
     { HttpExchange exchange ->	
+        def query = exchange.requestURI.query
         def url = [
             "https://",
             exchange.requestHeaders.getFirst("Host"),
-            exchange.requestURI.path
+            exchange.requestURI.path,
+            query ? "?"+query : "",
         ].join('')
 
         println(exchange.requestMethod + " - " + url)
         exchange.responseHeaders.set("Location", url)
         exchange.sendResponseHeaders(301, 0)
+        exchange.responseBody.close()
     } as HttpHandler
 )
 server.start()
